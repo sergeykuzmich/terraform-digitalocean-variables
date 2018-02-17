ec8dd7b4a96dc539516ede13d0858a82e7611ad4
Sergey Kuzmich <contact@kuzmi.ch>
2018-02-17 20:14:05 +0000

    Deploy sergeykuzmich/terraform-digitalocean-variables to github.com/sergeykuzmich/terraform-digitalocean-variables.git:master

---
+++ do_distributions.tf
@@ -0,0 +1,143 @@
+###########################
+##  Image Distributions
+#
+
+variable "do_distibution_coreos-beta" {
+  description = "CoreOS 1662.2.0 (beta)"
+  defalut     = "coreos-beta"
+}
+
+variable "do_distibution_freebsd-10-3-x64-zfs" {
+  description = "FreeBSD 10.3 x64 zfs"
+  defalut     = "freebsd-10-3-x64-zfs"
+}
+
+variable "do_distibution_freebsd-10-3-x64" {
+  description = "FreeBSD 10.3 x64"
+  defalut     = "freebsd-10-3-x64"
+}
+
+variable "do_distibution_freebsd-11-0-x64-zfs" {
+  description = "FreeBSD 11.0 x64 ZFS"
+  defalut     = "freebsd-11-0-x64-zfs"
+}
+
+variable "do_distibution_freebsd-11-0-x64" {
+  description = "FreeBSD 11.0 x64"
+  defalut     = "freebsd-11-0-x64"
+}
+
+variable "do_distibution_fedora-26-x64" {
+  description = "Fedora 26 x64"
+  defalut     = "fedora-26-x64"
+}
+
+variable "do_distibution_freebsd-11-1-x64" {
+  description = "FreeBSD 11.1 x64"
+  defalut     = "freebsd-11-1-x64"
+}
+
+variable "do_distibution_freebsd-11-1-x64-zfs" {
+  description = "FreeBSD 11.1 x64 ZFS"
+  defalut     = "freebsd-11-1-x64-zfs"
+}
+
+variable "do_distibution_debian-7-x64" {
+  description = "Debian 7.11 x64"
+  defalut     = "debian-7-x64"
+}
+
+variable "do_distibution_debian-7-x32" {
+  description = "Debian 7.11 x32"
+  defalut     = "debian-7-x32"
+}
+
+variable "do_distibution_freebsd-10-4-x64-zfs" {
+  description = "FreeBSD 10.4 x64 ZFS"
+  defalut     = "freebsd-10-4-x64-zfs"
+}
+
+variable "do_distibution_freebsd-10-4-x64" {
+  description = "FreeBSD 10.4 x64"
+  defalut     = "freebsd-10-4-x64"
+}
+
+variable "do_distibution_fedora-27-x64" {
+  description = "Fedora 27 x64"
+  defalut     = "fedora-27-x64"
+}
+
+variable "do_distibution_fedora-27-x64-atomic" {
+  description = "Fedora Atomic 27 Atomic x64"
+  defalut     = "fedora-27-x64-atomic"
+}
+
+variable "do_distibution_centos-7-x64" {
+  description = "CentOS 7.4 x64"
+  defalut     = "centos-7-x64"
+}
+
+variable "do_distibution_ubuntu-16-04-x32" {
+  description = "Ubuntu 16.04.3 x32"
+  defalut     = "ubuntu-16-04-x32"
+}
+
+variable "do_distibution_centos-6-x32" {
+  description = "CentOS 6.9 x32"
+  defalut     = "centos-6-x32"
+}
+
+variable "do_distibution_centos-6-x64" {
+  description = "CentOS 6.9 x64"
+  defalut     = "centos-6-x64"
+}
+
+variable "do_distibution_ubuntu-14-04-x64" {
+  description = "Ubuntu 14.04.5 x64"
+  defalut     = "ubuntu-14-04-x64"
+}
+
+variable "do_distibution_rancheros" {
+  description = "RancherOS v1.2.0 x64"
+  defalut     = "rancheros"
+}
+
+variable "do_distibution_debian-8-x64" {
+  description = "Debian 8.10 x64"
+  defalut     = "debian-8-x64"
+}
+
+variable "do_distibution_debian-8-x32" {
+  description = "Debian 8.10 x32"
+  defalut     = "debian-8-x32"
+}
+
+variable "do_distibution_ubuntu-14-04-x32" {
+  description = "Ubuntu 14.04.5 x32"
+  defalut     = "ubuntu-14-04-x32"
+}
+
+variable "do_distibution_ubuntu-16-04-x64" {
+  description = "Ubuntu 16.04.3 x64"
+  defalut     = "ubuntu-16-04-x64"
+}
+
+variable "do_distibution_debian-9-x64" {
+  description = "Debian 9.3 x64"
+  defalut     = "debian-9-x64"
+}
+
+variable "do_distibution_coreos-stable" {
+  description = "CoreOS 1632.3.0 (stable)"
+  defalut     = "coreos-stable"
+}
+
+variable "do_distibution_coreos-alpha" {
+  description = "CoreOS 1688.0.0 (alpha)"
+  defalut     = "coreos-alpha"
+}
+
+variable "do_distibution_ubuntu-17-10-x64" {
+  description = "Ubuntu 17.10 x64"
+  defalut     = "ubuntu-17-10-x64"
+}
---
+++ do_regions.tf
@@ -0,0 +1,48 @@
+############################
+##  DigitalOcean Regions
+#
+
+variable "do_region_nyc1" {
+  description = "New York 1"
+  defalut     = "nyc1"
+}
+
+variable "do_region_sgp1" {
+  description = "Singapore 1"
+  defalut     = "sgp1"
+}
+
+variable "do_region_lon1" {
+  description = "London 1"
+  defalut     = "lon1"
+}
+
+variable "do_region_nyc3" {
+  description = "New York 3"
+  defalut     = "nyc3"
+}
+
+variable "do_region_ams3" {
+  description = "Amsterdam 3"
+  defalut     = "ams3"
+}
+
+variable "do_region_fra1" {
+  description = "Frankfurt 1"
+  defalut     = "fra1"
+}
+
+variable "do_region_tor1" {
+  description = "Toronto 1"
+  defalut     = "tor1"
+}
+
+variable "do_region_sfo2" {
+  description = "San Francisco 2"
+  defalut     = "sfo2"
+}
+
+variable "do_region_blr1" {
+  description = "Bangalore 1"
+  defalut     = "blr1"
+}
