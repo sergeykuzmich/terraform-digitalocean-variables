6338b6abd54990d1d2751774e39f3299695ad4f6
Sergey Kuzmich <contact@kuzmi.ch>
2018-03-31 10:53:09 +0000

    Deploy sergeykuzmich/terraform-digitalocean-variables to github.com/sergeykuzmich/terraform-digitalocean-variables.git:master

--- do_distributions.tf
+++ do_distributions.tf
@@ -2,39 +2,34 @@
 ##  Image Distributions
 #
 
+variable "do_distibution_centos-6-x32" {
+  description = "CentOS 6.9 x32"
+  default     = "centos-6-x32"
+}
+
+variable "do_distibution_centos-6-x64" {
+  description = "CentOS 6.9 x64"
+  default     = "centos-6-x64"
+}
+
+variable "do_distibution_centos-7-x64" {
+  description = "CentOS 7.4 x64"
+  default     = "centos-7-x64"
+}
+
+variable "do_distibution_coreos-alpha" {
+  description = "CoreOS 1729.0.0 (alpha)"
+  default     = "coreos-alpha"
+}
+
 variable "do_distibution_coreos-beta" {
-  description = "CoreOS 1722.1.0 (beta)"
+  description = "CoreOS 1722.2.0 (beta)"
   default     = "coreos-beta"
 }
 
-variable "do_distibution_freebsd-10-3-x64-zfs" {
-  description = "FreeBSD 10.3 x64 zfs"
-  default     = "freebsd-10-3-x64-zfs"
-}
-
-variable "do_distibution_freebsd-10-3-x64" {
-  description = "FreeBSD 10.3 x64"
-  default     = "freebsd-10-3-x64"
-}
-
-variable "do_distibution_fedora-26-x64" {
-  description = "Fedora 26 x64"
-  default     = "fedora-26-x64"
-}
-
-variable "do_distibution_freebsd-11-1-x64" {
-  description = "FreeBSD 11.1 x64"
-  default     = "freebsd-11-1-x64"
-}
-
-variable "do_distibution_freebsd-11-1-x64-zfs" {
-  description = "FreeBSD 11.1 x64 ZFS"
-  default     = "freebsd-11-1-x64-zfs"
-}
-
-variable "do_distibution_debian-7-x64" {
-  description = "Debian 7.11 x64"
-  default     = "debian-7-x64"
+variable "do_distibution_coreos-stable" {
+  description = "CoreOS 1632.3.0 (stable)"
+  default     = "coreos-stable"
 }
 
 variable "do_distibution_debian-7-x32" {
@@ -42,14 +37,29 @@ variable "do_distibution_debian-7-x32" {
   default     = "debian-7-x32"
 }
 
-variable "do_distibution_freebsd-10-4-x64-zfs" {
-  description = "FreeBSD 10.4 x64 ZFS"
-  default     = "freebsd-10-4-x64-zfs"
+variable "do_distibution_debian-7-x64" {
+  description = "Debian 7.11 x64"
+  default     = "debian-7-x64"
 }
 
-variable "do_distibution_freebsd-10-4-x64" {
-  description = "FreeBSD 10.4 x64"
-  default     = "freebsd-10-4-x64"
+variable "do_distibution_debian-8-x32" {
+  description = "Debian 8.10 x32"
+  default     = "debian-8-x32"
+}
+
+variable "do_distibution_debian-8-x64" {
+  description = "Debian 8.10 x64"
+  default     = "debian-8-x64"
+}
+
+variable "do_distibution_debian-9-x64" {
+  description = "Debian 9.4 x64"
+  default     = "debian-9-x64"
+}
+
+variable "do_distibution_fedora-26-x64" {
+  description = "Fedora 26 x64"
+  default     = "fedora-26-x64"
 }
 
 variable "do_distibution_fedora-27-x64" {
@@ -62,14 +72,34 @@ variable "do_distibution_fedora-27-x64-atomic" {
   default     = "fedora-27-x64-atomic"
 }
 
-variable "do_distibution_centos-6-x32" {
-  description = "CentOS 6.9 x32"
-  default     = "centos-6-x32"
+variable "do_distibution_freebsd-10-3-x64" {
+  description = "FreeBSD 10.3 x64"
+  default     = "freebsd-10-3-x64"
 }
 
-variable "do_distibution_centos-6-x64" {
-  description = "CentOS 6.9 x64"
-  default     = "centos-6-x64"
+variable "do_distibution_freebsd-10-3-x64-zfs" {
+  description = "FreeBSD 10.3 x64 zfs"
+  default     = "freebsd-10-3-x64-zfs"
+}
+
+variable "do_distibution_freebsd-10-4-x64" {
+  description = "FreeBSD 10.4 x64"
+  default     = "freebsd-10-4-x64"
+}
+
+variable "do_distibution_freebsd-10-4-x64-zfs" {
+  description = "FreeBSD 10.4 x64 ZFS"
+  default     = "freebsd-10-4-x64-zfs"
+}
+
+variable "do_distibution_freebsd-11-1-x64" {
+  description = "FreeBSD 11.1 x64"
+  default     = "freebsd-11-1-x64"
+}
+
+variable "do_distibution_freebsd-11-1-x64-zfs" {
+  description = "FreeBSD 11.1 x64 ZFS"
+  default     = "freebsd-11-1-x64-zfs"
 }
 
 variable "do_distibution_rancheros" {
@@ -77,24 +107,9 @@ variable "do_distibution_rancheros" {
   default     = "rancheros"
 }
 
-variable "do_distibution_debian-8-x64" {
-  description = "Debian 8.10 x64"
-  default     = "debian-8-x64"
-}
-
-variable "do_distibution_debian-8-x32" {
-  description = "Debian 8.10 x32"
-  default     = "debian-8-x32"
-}
-
-variable "do_distibution_ubuntu-16-04-x32" {
-  description = "Ubuntu 16.04.4 x32"
-  default     = "ubuntu-16-04-x32"
-}
-
-variable "do_distibution_centos-7-x64" {
-  description = "CentOS 7.4 x64"
-  default     = "centos-7-x64"
+variable "do_distibution_ubuntu-14-04-x32" {
+  description = "Ubuntu 14.04.5 x32"
+  default     = "ubuntu-14-04-x32"
 }
 
 variable "do_distibution_ubuntu-14-04-x64" {
@@ -102,32 +117,17 @@ variable "do_distibution_ubuntu-14-04-x64" {
   default     = "ubuntu-14-04-x64"
 }
 
+variable "do_distibution_ubuntu-16-04-x32" {
+  description = "Ubuntu 16.04.4 x32"
+  default     = "ubuntu-16-04-x32"
+}
+
 variable "do_distibution_ubuntu-16-04-x64" {
   description = "Ubuntu 16.04.4 x64"
   default     = "ubuntu-16-04-x64"
 }
 
-variable "do_distibution_ubuntu-14-04-x32" {
-  description = "Ubuntu 14.04.5 x32"
-  default     = "ubuntu-14-04-x32"
-}
-
-variable "do_distibution_debian-9-x64" {
-  description = "Debian 9.4 x64"
-  default     = "debian-9-x64"
-}
-
 variable "do_distibution_ubuntu-17-10-x64" {
   description = "Ubuntu 17.10 x64"
   default     = "ubuntu-17-10-x64"
-}
-
-variable "do_distibution_coreos-stable" {
-  description = "CoreOS 1688.4.0 (stable)"
-  default     = "coreos-stable"
-}
-
-variable "do_distibution_coreos-alpha" {
-  description = "CoreOS 1729.0.0 (alpha)"
-  default     = "coreos-alpha"
 }
