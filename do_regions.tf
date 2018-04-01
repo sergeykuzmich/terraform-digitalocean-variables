b3be353ab75a093cb4a8bfd49d33465e4fe07de1
Sergey Kuzmich <contact@kuzmi.ch>
2018-04-01 10:44:18 +0000

    Deploy sergeykuzmich/terraform-digitalocean-variables to github.com/sergeykuzmich/terraform-digitalocean-variables.git:master

--- do_regions.tf
+++ do_regions.tf
@@ -2,29 +2,14 @@
 ##  DigitalOcean Regions
 #
 
-variable "do_region_nyc1" {
-  description = "New York 1"
-  default     = "nyc1"
-}
-
-variable "do_region_sgp1" {
-  description = "Singapore 1"
-  default     = "sgp1"
-}
-
-variable "do_region_lon1" {
-  description = "London 1"
-  default     = "lon1"
-}
-
-variable "do_region_nyc3" {
-  description = "New York 3"
-  default     = "nyc3"
-}
-
 variable "do_region_ams3" {
   description = "Amsterdam 3"
   default     = "ams3"
+}
+
+variable "do_region_blr1" {
+  description = "Bangalore 1"
+  default     = "blr1"
 }
 
 variable "do_region_fra1" {
@@ -32,9 +17,19 @@ variable "do_region_fra1" {
   default     = "fra1"
 }
 
-variable "do_region_tor1" {
-  description = "Toronto 1"
-  default     = "tor1"
+variable "do_region_lon1" {
+  description = "London 1"
+  default     = "lon1"
+}
+
+variable "do_region_nyc1" {
+  description = "New York 1"
+  default     = "nyc1"
+}
+
+variable "do_region_nyc3" {
+  description = "New York 3"
+  default     = "nyc3"
 }
 
 variable "do_region_sfo2" {
@@ -42,7 +37,12 @@ variable "do_region_sfo2" {
   default     = "sfo2"
 }
 
-variable "do_region_blr1" {
-  description = "Bangalore 1"
-  default     = "blr1"
+variable "do_region_sgp1" {
+  description = "Singapore 1"
+  default     = "sgp1"
+}
+
+variable "do_region_tor1" {
+  description = "Toronto 1"
+  default     = "tor1"
 }
--- do_sizes.tf
+++ do_sizes.tf
@@ -2,19 +2,94 @@
 ##  Droplet Sizes
 #
 
-variable "do_size_512mb" {
-  description = "512MB RAM; 1 CPU; 20GB SSD - $5/mo"
-  default     = "512mb"
-}
-
-variable "do_size_s-1vcpu-1gb" {
-  description = "1GB RAM; 1 CPU; 25GB SSD - $5/mo"
-  default     = "s-1vcpu-1gb"
+variable "do_size_16gb" {
+  description = "16GB RAM; 8 CPU; 160GB SSD - $160/mo"
+  default     = "16gb"
 }
 
 variable "do_size_1gb" {
   description = "1GB RAM; 1 CPU; 30GB SSD - $10/mo"
   default     = "1gb"
+}
+
+variable "do_size_2gb" {
+  description = "2GB RAM; 2 CPU; 40GB SSD - $20/mo"
+  default     = "2gb"
+}
+
+variable "do_size_32gb" {
+  description = "32GB RAM; 12 CPU; 320GB SSD - $320/mo"
+  default     = "32gb"
+}
+
+variable "do_size_48gb" {
+  description = "48GB RAM; 16 CPU; 480GB SSD - $480/mo"
+  default     = "48gb"
+}
+
+variable "do_size_4gb" {
+  description = "4GB RAM; 2 CPU; 60GB SSD - $40/mo"
+  default     = "4gb"
+}
+
+variable "do_size_512mb" {
+  description = "512MB RAM; 1 CPU; 20GB SSD - $5/mo"
+  default     = "512mb"
+}
+
+variable "do_size_64gb" {
+  description = "64GB RAM; 20 CPU; 640GB SSD - $640/mo"
+  default     = "64gb"
+}
+
+variable "do_size_8gb" {
+  description = "8GB RAM; 4 CPU; 80GB SSD - $80/mo"
+  default     = "8gb"
+}
+
+variable "do_size_c-16" {
+  description = "32GB RAM; 16 CPU; 200GB SSD - $320/mo"
+  default     = "c-16"
+}
+
+variable "do_size_c-2" {
+  description = "4GB RAM; 2 CPU; 25GB SSD - $40/mo"
+  default     = "c-2"
+}
+
+variable "do_size_c-32" {
+  description = "64GB RAM; 32 CPU; 400GB SSD - $640/mo"
+  default     = "c-32"
+}
+
+variable "do_size_c-4" {
+  description = "8GB RAM; 4 CPU; 50GB SSD - $80/mo"
+  default     = "c-4"
+}
+
+variable "do_size_c-48" {
+  description = "72GB RAM; 48 CPU; 20GB SSD - $960/mo"
+  default     = "c-48"
+}
+
+variable "do_size_c-8" {
+  description = "16GB RAM; 8 CPU; 100GB SSD - $160/mo"
+  default     = "c-8"
+}
+
+variable "do_size_s-12vcpu-48gb" {
+  description = "48GB RAM; 12 CPU; 960GB SSD - $240/mo"
+  default     = "s-12vcpu-48gb"
+}
+
+variable "do_size_s-16vcpu-64gb" {
+  description = "64GB RAM; 16 CPU; 1.25TB SSD - $320/mo"
+  default     = "s-16vcpu-64gb"
+}
+
+variable "do_size_s-1vcpu-1gb" {
+  description = "1GB RAM; 1 CPU; 25GB SSD - $5/mo"
+  default     = "s-1vcpu-1gb"
 }
 
 variable "do_size_s-1vcpu-2gb" {
@@ -27,109 +102,9 @@ variable "do_size_s-1vcpu-3gb" {
   default     = "s-1vcpu-3gb"
 }
 
-variable "do_size_s-2vcpu-2gb" {
-  description = "2GB RAM; 2 CPU; 60GB SSD - $15/mo"
-  default     = "s-2vcpu-2gb"
-}
-
-variable "do_size_s-3vcpu-1gb" {
-  description = "1GB RAM; 3 CPU; 60GB SSD - $15/mo"
-  default     = "s-3vcpu-1gb"
-}
-
-variable "do_size_2gb" {
-  description = "2GB RAM; 2 CPU; 40GB SSD - $20/mo"
-  default     = "2gb"
-}
-
-variable "do_size_s-2vcpu-4gb" {
-  description = "4GB RAM; 2 CPU; 80GB SSD - $20/mo"
-  default     = "s-2vcpu-4gb"
-}
-
-variable "do_size_4gb" {
-  description = "4GB RAM; 2 CPU; 60GB SSD - $40/mo"
-  default     = "4gb"
-}
-
-variable "do_size_c-2" {
-  description = "4GB RAM; 2 CPU; 25GB SSD - $40/mo"
-  default     = "c-2"
-}
-
-variable "do_size_s-4vcpu-8gb" {
-  description = "8GB RAM; 4 CPU; 160GB SSD - $40/mo"
-  default     = "s-4vcpu-8gb"
-}
-
-variable "do_size_8gb" {
-  description = "8GB RAM; 4 CPU; 80GB SSD - $80/mo"
-  default     = "8gb"
-}
-
-variable "do_size_c-4" {
-  description = "8GB RAM; 4 CPU; 50GB SSD - $80/mo"
-  default     = "c-4"
-}
-
-variable "do_size_s-6vcpu-16gb" {
-  description = "16GB RAM; 6 CPU; 320GB SSD - $80/mo"
-  default     = "s-6vcpu-16gb"
-}
-
-variable "do_size_16gb" {
-  description = "16GB RAM; 8 CPU; 160GB SSD - $160/mo"
-  default     = "16gb"
-}
-
-variable "do_size_c-8" {
-  description = "16GB RAM; 8 CPU; 100GB SSD - $160/mo"
-  default     = "c-8"
-}
-
-variable "do_size_s-8vcpu-32gb" {
-  description = "32GB RAM; 8 CPU; 640GB SSD - $160/mo"
-  default     = "s-8vcpu-32gb"
-}
-
-variable "do_size_s-12vcpu-48gb" {
-  description = "48GB RAM; 12 CPU; 960GB SSD - $240/mo"
-  default     = "s-12vcpu-48gb"
-}
-
-variable "do_size_32gb" {
-  description = "32GB RAM; 12 CPU; 320GB SSD - $320/mo"
-  default     = "32gb"
-}
-
-variable "do_size_c-16" {
-  description = "32GB RAM; 16 CPU; 200GB SSD - $320/mo"
-  default     = "c-16"
-}
-
-variable "do_size_s-16vcpu-64gb" {
-  description = "64GB RAM; 16 CPU; 1.25TB SSD - $320/mo"
-  default     = "s-16vcpu-64gb"
-}
-
-variable "do_size_48gb" {
-  description = "48GB RAM; 16 CPU; 480GB SSD - $480/mo"
-  default     = "48gb"
-}
-
 variable "do_size_s-20vcpu-96gb" {
   description = "96GB RAM; 20 CPU; 1.875TB SSD - $480/mo"
   default     = "s-20vcpu-96gb"
-}
-
-variable "do_size_64gb" {
-  description = "64GB RAM; 20 CPU; 640GB SSD - $640/mo"
-  default     = "64gb"
-}
-
-variable "do_size_c-32" {
-  description = "64GB RAM; 32 CPU; 400GB SSD - $640/mo"
-  default     = "c-32"
 }
 
 variable "do_size_s-24vcpu-128gb" {
@@ -137,12 +112,37 @@ variable "do_size_s-24vcpu-128gb" {
   default     = "s-24vcpu-128gb"
 }
 
-variable "do_size_c-48" {
-  description = "72GB RAM; 48 CPU; 20GB SSD - $960/mo"
-  default     = "c-48"
+variable "do_size_s-2vcpu-2gb" {
+  description = "2GB RAM; 2 CPU; 60GB SSD - $15/mo"
+  default     = "s-2vcpu-2gb"
+}
+
+variable "do_size_s-2vcpu-4gb" {
+  description = "4GB RAM; 2 CPU; 80GB SSD - $20/mo"
+  default     = "s-2vcpu-4gb"
 }
 
 variable "do_size_s-32vcpu-192gb" {
   description = "192GB RAM; 32 CPU; 3.75TB SSD - $960/mo"
   default     = "s-32vcpu-192gb"
+}
+
+variable "do_size_s-3vcpu-1gb" {
+  description = "1GB RAM; 3 CPU; 60GB SSD - $15/mo"
+  default     = "s-3vcpu-1gb"
+}
+
+variable "do_size_s-4vcpu-8gb" {
+  description = "8GB RAM; 4 CPU; 160GB SSD - $40/mo"
+  default     = "s-4vcpu-8gb"
+}
+
+variable "do_size_s-6vcpu-16gb" {
+  description = "16GB RAM; 6 CPU; 320GB SSD - $80/mo"
+  default     = "s-6vcpu-16gb"
+}
+
+variable "do_size_s-8vcpu-32gb" {
+  description = "32GB RAM; 8 CPU; 640GB SSD - $160/mo"
+  default     = "s-8vcpu-32gb"
 }
