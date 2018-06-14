b30e0f0bea08dde49d16766fed07f0252e923fec
Sergey Kuzmich <contact@kuzmi.ch>
2018-06-14 21:24:30 +0000

    Deploy sergeykuzmich/terraform-digitalocean-variables to github.com/sergeykuzmich/terraform-digitalocean-variables.git:master

--- do_distributions.tf
+++ do_distributions.tf
@@ -103,7 +103,7 @@ variable "do_distibution_freebsd-11-1-x64-zfs" {
 }
 
 variable "do_distibution_rancheros" {
-  description = "RancherOS v1.3.0"
+  description = "RancherOS v1.4.0"
   default     = "rancheros"
 }
 
