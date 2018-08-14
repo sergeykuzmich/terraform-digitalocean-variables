db61024fffe97082dc51e7705911ce34b4a10a90
Sergey Kuzmich <contact@kuzmi.ch>
2018-08-14 21:56:49 +0000

    Deploy sergeykuzmich/terraform-digitalocean-variables to github.com/sergeykuzmich/terraform-digitalocean-variables.git:master

--- do_distributions.tf
+++ do_distributions.tf
@@ -118,16 +118,16 @@ variable "do_distibution_ubuntu-14-04-x64" {
 }
 
 variable "do_distibution_ubuntu-16-04-x32" {
-  description = "Ubuntu 16.04.5 x32"
+  description = "Ubuntu 16.04.4 x32"
   default     = "ubuntu-16-04-x32"
 }
 
 variable "do_distibution_ubuntu-16-04-x64" {
-  description = "Ubuntu 16.04.5 x64"
+  description = "Ubuntu 16.04.4 x64"
   default     = "ubuntu-16-04-x64"
 }
 
 variable "do_distibution_ubuntu-18-04-x64" {
-  description = "Ubuntu 18.04.1 x64"
+  description = "Ubuntu 18.04 x64"
   default     = "ubuntu-18-04-x64"
 }
