aa886fe98ab76fbb6c5b0ee7d8504040c9baf46a
Sergey Kuzmich <contact@kuzmi.ch>
2018-08-29 22:04:05 +0000

    Deploy sergeykuzmich/terraform-digitalocean-variables to github.com/sergeykuzmich/terraform-digitalocean-variables.git:master

--- do_distributions.tf
+++ do_distributions.tf
@@ -18,7 +18,7 @@ variable "do_distibution_centos-7-x64" {
 }
 
 variable "do_distibution_coreos-alpha" {
-  description = "CoreOS 1871.0.0 (alpha)"
+  description = "CoreOS 1883.0.0 (alpha)"
   default     = "coreos-alpha"
 }
 
