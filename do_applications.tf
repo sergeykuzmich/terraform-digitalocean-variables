18dda0b3a3c75e629d71e8bc3eb3afb9ed79d9a8
Sergey Kuzmich <contact@kuzmi.ch>
2019-12-07 02:00:55 +0000

    Deploy sergeykuzmich/terraform-digitalocean-variables to github.com/sergeykuzmich/terraform-digitalocean-variables.git:master

--- do_applications.tf
+++ do_applications.tf
@@ -18,8 +18,13 @@ variable "do_application_alfio-7-6" {
 }
 
 variable "do_application_antmedia-16-04" {
-  description = "AntMedia Server CE on 16.04"
+  description = "AntMedia Server CE 1.9.0 on Ubuntu 16.04"
   default     = "antmedia-16-04"
+}
+
+variable "do_application_antmedia-antmediaserveren-16-04" {
+  description = "AntMedia Server Enterprise Edition 1.9.0 on Ubuntu 16.04"
+  default     = "antmedia-antmediaserveren-16-04"
 }
 
 variable "do_application_apisnetworks-apnscp-7-7" {
@@ -447,24 +452,9 @@ variable "do_application_thingsboardpe-18-04" {
   default     = "thingsboardpe-18-04"
 }
 
-variable "do_application_weconexpbx-7-6" {
-  description = "weconex PBX 3"
-  default     = "weconexpbx-7-6"
-}
-
-variable "do_application_wordpress-18-04" {
-  description = "WordPress on 18.04"
-  default     = "wordpress-18-04"
-}
-
 variable "do_application_zabbix-7-6" {
   description = "Zabbix 4.2.0 on 7.6"
   default     = "zabbix-7-6"
-}
-
-variable "do_application_zeromon-zabbix-18-04" {
-  description = "Zeromon Zabbix 4 on Ubuntu 18.04"
-  default     = "zeromon-zabbix-18-04"
 }
 
 variable "do_application_null" {
@@ -485,6 +475,21 @@ variable "do_application_vardot-varbase-18-04" {
 variable "do_application_vitalpointz-7-6" {
   description = "vitalpointz IoT Core Lite 1.1.0 on CentOS 7.6"
   default     = "vitalpointz-7-6"
+}
+
+variable "do_application_weconexpbx-7-6" {
+  description = "weconex PBX 3"
+  default     = "weconexpbx-7-6"
+}
+
+variable "do_application_wordpress-18-04" {
+  description = "WordPress on 18.04"
+  default     = "wordpress-18-04"
+}
+
+variable "do_application_zeromon-zabbix-18-04" {
+  description = "Zeromon Zabbix 4 on Ubuntu 18.04"
+  default     = "zeromon-zabbix-18-04"
 }
 
 variable "do_application_zulip-18-04" {
